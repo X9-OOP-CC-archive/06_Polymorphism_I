a627dc912ffd291e247ed9f5df1232008b0263cc
Peter <schmidt.multimedia@gmail.com>
2023-04-25 10:25:20 +0200

    1. Variante . Overloading

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,20 +8,28 @@ public class App {
         Worker worker = new Worker();
         Drone drone = new Drone();
 
-        output(queen.doYourJob());
-        output(queen.fly());
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
 
-        output(worker.doYourJob());
-        output(worker.fly());
-
-        output(drone.doYourJob());
-        output(drone.fly());
-   
-        
     }
 
-   
-   
+   // 1. Variante . Overloading - überladene Methoden
+    private static void pollObj(Queen obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+    }
+
+    private static void pollObj(Worker obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+    }
+
+    private static void pollObj(Drone obj) {
+        output(obj.doYourJob());
+        output(obj.fly());
+    }
+
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
